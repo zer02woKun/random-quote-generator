060c99e0ad4e3da6b7659c903d7aaa6c1826ee78
ren ren <zerotwo.alanofren@outlook.com>
2025-08-11 20:31:03 +0800

    Add random styles to generated quotes

--- randomQuotesGenerator.py
+++ randomQuotesGenerator.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import QMainWindow, QGridLayout, QHBoxLayout, QVBoxLayout, 
 from PyQt5.QtCore import Qt
 import sys
 import requests
-
+import random
 
 
 class MainWindow(QMainWindow):
@@ -12,6 +12,8 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Random Quote Generator")
         
         self.textBox = QTextEdit()
+        self.textBox.setObjectName("TextScreen")
+        self.textBox.setAlignment(Qt.AlignCenter)
         self.button = QPushButton("Generate")
         self.button.setObjectName("Ok")
         self.button.clicked.connect(self.getRandomQuote)
@@ -40,7 +42,7 @@ class MainWindow(QMainWindow):
         hBox.addWidget(self.textBox)
         
         self.textBox.setReadOnly(True)
-        self.textBox.setAlignment(Qt.AlignCenter)
+        
 
         hBox1 = QHBoxLayout()
         self.button.setMaximumSize(300, 100)
@@ -48,10 +50,6 @@ class MainWindow(QMainWindow):
         hBox1.addWidget(self.button1,0)
         hBox1.addWidget(self.button,1)
         
-    
-
-        
-
 
         mainLayout.addLayout(hBox)
         mainLayout.addSpacing(50)
@@ -72,25 +70,35 @@ class MainWindow(QMainWindow):
 """)
 
     #get a random quote on the zenquotes website, then update the textbox
-    def getRandomQuote(self):
+    def getRandomQuote(self)-> None:
         url = "https://zenquotes.io/api/random"
         response = requests.get(url)
 
         data = response.json()
         quote = data[0]['q']
         author = data[0]['a']
+
         self.textBox.setPlainText(f"{quote} -{author}")
+        self.randomizeStyles()
         print(quote)
 
-
+    #copies the quote then add it to the clipboard
     def copyToClipboard(self):
         clipboard = QApplication.clipboard()
         clipboard.setText(self.textBox.toPlainText())
         print("copied")
+    #gives the quote a random styles
+    def randomizeStyles(self)-> None:
+        fonts = ["Courier New","Georgia", "Papyrus"]
+        font = random.choice(fonts)
+        print(font)
+        self.textBox.setStyleSheet(f"font-family:{font};")
+        
 
 
 def main():
     app = QApplication(sys.argv)
+    app.setStyle('Fusion')
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
